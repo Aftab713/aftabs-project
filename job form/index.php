04add21bd7da1fa48f2a05ee1adb3c7e46d2a4c7
Aftab713 <67420157+Aftab713@users.noreply.github.com>
2020-06-25 17:05:46 +0500

    Add files via upload

---
+++ job form/index.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+    <title>Job Application Form</title>
+    <link rel="stylesheet" type="text/css" href="Assets/css/style.css">
+
+</head>
+
+<body>
+
+     <div class="mkn-app">
+     <form class="mkn-formbox">
+         <h1>Employment Application Form</h1>
+             <div class="mkn-print">PLEASE PRINT ALL INFORMATION REQUESTED EXCEPT SIGNATURES</div>
+
+
+         <div class="mkn-drug"><h3>APPLICANTS MAY BE TESTED FOR ILLEGAL DRUGS</h3>
+         </div>
+
+         <div class="mkn-2">
+             PLEASE COMPLETE PAGES 1-5.
+             <h5> DATE:</h5>
+             <h4>NAME:</h4>
+             </div>
+
+         <div class="mkn-name">
+
+             <div class="row">
+                 <div class="column">
+                     <h6>Last</h6>
+                 </div>
+
+                 <div class="column">
+                     <h6>First</h6>
+                 </div>
+
+                 <div class="column">
+                     <h6>Middle</h6>
+                 </div>
+
+                 <div class="column">
+                     <h6>Maiden</h6>
+                 </div>
+             </div>
+
+             </div>
+     </form>
+
+
+     </div>
+
+
+</body>
+
+</html>
